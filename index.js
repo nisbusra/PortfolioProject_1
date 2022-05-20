021b3536fb51fa8ca61133e0420b0259b5bca2e6
nisbusra <busranisanci@outlook.com>
2022-05-21 01:04:28 +0300

    bg changes in div

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ function handleClick1(ev) {
     alert('Feel free to contact :)');
 }
 
-const clicking= document.querySelector('.renk');
+const divv= document.querySelector('.renk')
+divv.addEventListener('click', function onClick(event){
+    event.target.style.backgroundColor = 'white'
+})
 
-clicking.addEventListener('click', function onclick(event){
-    document('.renk').style.backgroundColor = 'White';
-}) 
+let classarray = ['htmlicon', 'cssicon', 'jsicon', 'tailwindicon', 'githubicon', 'bootstrapicon', 'figmaicon']
+
+const linkss = ['https://html.com/', 'https://www.w3.org/Style/CSS/', 'https://www.javascript.com/', 'https://tailwindcss.com/', 'https://github.com/', 'https://getbootstrap.com/', 'https://www.figma.com/']
+
