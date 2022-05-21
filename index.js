8bf0d2c084f59441066129166b9d7bbe7cba1c2f
nisbusra <busranisanci@outlook.com>
2022-05-22 00:01:18 +0300

    JS alert changes

--- index.js
+++ index.js
@@ -16,36 +16,56 @@ divv.addEventListener('click', function onClick(event){
 
 //iconlar alert//
 
-const icon1 = document.querySelector('.htmlicon')
-icon1.addEventListener('click', function onClick(event){
-    alert('https://html.com/')
-})
+// const icon1 = document.querySelector('.htmlicon')
+// icon1.addEventListener('click', function onClick(event){
+//     alert('https://html.com/')
+// })
 
-const icon2 = document.querySelector('.cssicon', )
-icon2.addEventListener('click', function onClick(event){
-    alert('https://https://www.w3.org/Style/CSS/Overview.en.html.com/')
-})
+// const icon2 = document.querySelector('.cssicon', )
+// icon2.addEventListener('click', function onClick(event){
+//     alert('https://https://www.w3.org/Style/CSS/Overview.en.html.com/')
+// })
 
-const icon3 = document.querySelector('.jsicon')
-icon3.addEventListener('click', function onClick(event){
-    alert('https://www.javascript.com/')
-})
+// const icon3 = document.querySelector('.jsicon')
+// icon3.addEventListener('click', function onClick(event){
+//     alert('https://www.javascript.com/')
+// })
 
-const icon4 = document.querySelector('.taildwindicon')
-icon4.addEventListener('click', function onClick(event){
-    alert('https://tailwindcss.com/')
-})
-const icon5 = document.querySelector('.githubicon')
-icon5.addEventListener('click', function onClick(event){
-    alert('https://github.com/')
-})
+// const icon4 = document.querySelector('.taildwindicon')
+// icon4.addEventListener('click', function onClick(event){
+//     alert('https://tailwindcss.com/')
+// })
+// const icon5 = document.querySelector('.githubicon')
+// icon5.addEventListener('click', function onClick(event){
+//     alert('https://github.com/')
+// })
 
-const icon6 = document.querySelector('.bootstrapicon')
-icon6.addEventListener('click', function onClick(event){
-    alert('https://getbootstrap.com/')
-})
-const icon7 = document.querySelector('.figmaicon')
-icon7.addEventListener('click', function onClick(event){
-    alert('https://https://www.figma.com/.com/')
-})
+// const icon6 = document.querySelector('.bootstrapicon')
+// icon6.addEventListener('click', function onClick(event){
+//     alert('https://getbootstrap.com/')
+// })
+// const icon7 = document.querySelector('.figmaicon')
+// icon7.addEventListener('click', function onClick(event){
+//     alert('https://https://www.figma.com/.com/')
+// })
 
+//Best Alternatiffff//
+
+const ikons = document.querySelectorAll('.icon-alert')
+const links = {
+    CSS:'https://https://www.w3.org/Style/CSS/Overview.en.html.com/',
+    HTML:'https://html.com/',
+    Figma:'https://https://www.figma.com/.com/',
+    BootStrap:'https://getbootstrap.com/',
+    GitHub:'https://github.com/',
+    TailWindCss:'https://tailwindcss.com/',
+    JavaScript:'https://www.javascript.com/'
+}
+for(i=0; i<ikons.length; i++){
+    const element = ikons[i]
+    const attr = element.querySelector('img').getAttribute('alt')
+    element.addEventListener('click', function onClick(){
+        alert(links[attr])
+    })
+        
+}
