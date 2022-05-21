5e830946688169afd080000c68fb9edd56ca8cbb
nisbusra <busranisanci@outlook.com>
2022-05-21 22:09:52 +0300

    icons alert

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+// Let's do this part alert and bg changes //
 document.addEventListener(`DOMContentLoaded`, ()=> {
     document.querySelector('#letsdothis').addEventListener('click',handleClick1);
 }
@@ -13,7 +14,38 @@ divv.addEventListener('click', function onClick(event){
     event.target.style.backgroundColor = 'white'
 })
 
-let classarray = ['htmlicon', 'cssicon', 'jsicon', 'tailwindicon', 'githubicon', 'bootstrapicon', 'figmaicon']
+//iconlar alert//
 
-const linkss = ['https://html.com/', 'https://www.w3.org/Style/CSS/', 'https://www.javascript.com/', 'https://tailwindcss.com/', 'https://github.com/', 'https://getbootstrap.com/', 'https://www.figma.com/']
+const icon1 = document.querySelector('.htmlicon')
+icon1.addEventListener('click', function onClick(event){
+    alert('https://html.com/')
+})
+
+const icon2 = document.querySelector('.cssicon', )
+icon2.addEventListener('click', function onClick(event){
+    alert('https://https://www.w3.org/Style/CSS/Overview.en.html.com/')
+})
+
+const icon3 = document.querySelector('.jsicon')
+icon3.addEventListener('click', function onClick(event){
+    alert('https://www.javascript.com/')
+})
+
+const icon4 = document.querySelector('.taildwindicon')
+icon4.addEventListener('click', function onClick(event){
+    alert('https://tailwindcss.com/')
+})
+const icon5 = document.querySelector('.githubicon')
+icon5.addEventListener('click', function onClick(event){
+    alert('https://github.com/')
+})
+
+const icon6 = document.querySelector('.bootstrapicon')
+icon6.addEventListener('click', function onClick(event){
+    alert('https://getbootstrap.com/')
+})
+const icon7 = document.querySelector('.figmaicon')
+icon7.addEventListener('click', function onClick(event){
+    alert('https://https://www.figma.com/.com/')
+})
 
